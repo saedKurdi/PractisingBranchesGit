d7f47da2b7f0f79676b02a5aa52df33a5be6a9d0
Saed Mamedov <seid13463@gmail.com>
2024-11-03 18:02:08 +0400

    Saed's second commit

---
+++ GitTask/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitTask.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    public IActionResult GetWord()
+    {
+        return Ok("Salam");
+    }
+
+    public IActionResult GetWord2()
+    {
+        return Ok("Sagol");
+    }
+}
--- GitTask/obj/Debug/net8.0/GitTask.AssemblyInfo.cs
+++ GitTask/obj/Debug/net8.0/GitTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f4565e20a0164f23c7ca7efbe556f0d9f4356e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
